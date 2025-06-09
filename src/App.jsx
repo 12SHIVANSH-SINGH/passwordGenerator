37e5fa77fa972c42f7ec0e6797022d4e8bd0fb33
shivanshsingh <12shivanshsingha@gmail.com>
2025-06-09 14:19:26 +0530

    added some useState user freindly ui

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   const [digit, setDigit] = useState(false);
   const [sc, setSc] = useState(false);
   const [password, setPassword] = useState("SHivansH")
+  const [copy, setCopy] = useState("copy");
   // now the code is correct and is working but we are i=one state behind because useState is asynchronous in nature and this since it is not a promise we cannot even use promises.
   // hence this brings useEffect into play
 
@@ -21,11 +22,17 @@ function App() {
     
   }
 
+  const toggleCopy = () =>{
+    if(copy=="copied") return ; // if already copied then no need to copy
+    setCopy(((copy==="copy") ? "copied" : "copy")); // e;se toggle it 
+  }
   const passwordRef = useRef(null); // stores the refernce of password and we can use it to make some changes without the need of re rendering
   const copyPass = () => {
+    
     window.navigator.clipboard.writeText(password);
     passwordRef.current.select();
   }
+
 
   const toggleLenght = (e)=>{ // callback with a target to set the value
     setLength(e.target.value);
@@ -44,6 +51,8 @@ function App() {
       newPassword=newPassword+newField[(Math.floor(Math.random()*newField.length))]
     }
     setPassword(newPassword);
+    setCopy("copy"); // if password is changed then need to be switch to copy and since inside useeffect will do the task
+
     
   },[sc, digit, length, setPassword])
 
@@ -51,8 +60,8 @@ function App() {
     <div className="bg-stone-600 text-center w-full h-100 rounded-3xl p-2 m-3 border-3 ">
       <header className="block">Password Generator</header>
       <input className="bg-orange-300 text-black rounded-xl m-auto my-4 w-100 p-2 " ref={passwordRef} value={`${password}`} readOnly/>
-      <button className="bg-blue-400 p-2 rounded-xl w-20 mx-2 hover:bg-blue-600 cursor-pointer" onClick={copyPass}>
-        Copy
+      <button className="bg-blue-400 p-2 rounded-xl w-20 mx-2 hover:bg-blue-600 cursor-pointer" onClick={() => { copyPass();  toggleCopy();}}>
+        {copy}
       </button>
       <br />
       <input id="range" type="range" value={length} min={4} max={24} onChange={toggleLenght}/>
